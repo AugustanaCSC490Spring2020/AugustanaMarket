7b69fcf8fbd8242a62c41180fe4248c031d2e972
JConnors123 <jacobconnors32@gmail.com>
2020-04-13 23:03:02 -0500

    Names (Ehren, Jacob) -> We created an item listing page and linked it to redux. Added reducer and action for item list. Updated route for itemList

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+export {populate, add} from './listActions'
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import {firebaseReducer} from 'react-redux-firebase';
 import {firestoreReducer} from 'redux-firestore';
+import listReducer from './listReducer'
 
 const allReducers = combineReducers({
     firebase: firebaseReducer,
     firestore: firestoreReducer,
+    listReducer: listReducer,
 });
 
 export default allReducers;
