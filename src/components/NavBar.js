a5d805eaf5b382a02e31ef5e6dc7ba536596231e
Lars Harvey <larsenharvey16@augustana.edu>
2020-04-16 10:09:59 -0500

    Navbar appearance updated & tab icon/text added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,22 @@ const NavBar = () => {
     const firebase = useFirebase();
     const handleSignout = () => {
         firebase.logout();
-    }
+    };
     return (
         <body id={"navbar-body"}>
-            <div id={"navbar-div"}>
-                <Link to='/login'><button id="signout-btn" onClick={handleSignout}>Sign Out</button></Link>
-            </div>
+        <nav className="navbar justify-content-end fixed-top navbar-div">
+            <form className="form-inline">
+                <img src={"./textbook_2x.png"} id={"nav-img"}/>
+                <a className="navbar-brand">Augustana Marketplace</a>
+                <div id={"tester"}>
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-info" type="submit">Search</button>
+                    <button className="form-inline" id="signout-btn" onClick={handleSignout}>Sign Out</button>
+                </div>
+            </form>
+        </nav>
         </body>
     )
-}
+};
 
 export default NavBar;
