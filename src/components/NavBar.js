2cab6467ffbaabb14b616584920d7131061fdb64
Lars Harvey <larsenharvey16@augustana.edu>
2020-04-20 10:48:32 -0500

    Navbar styling fixed so with new nav links everything sits on one line

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,16 +14,14 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg fixed-top navbar-div">
             <form className="form-inline">
                 <img src={"../textbook_2x.png"} id={"nav-img"}/>
-                <a className="navbar-brand" id={"navbar-title"}>Augustana Marketplace</a>
+                <a className="navbar-brand" id={"navbar-title"} href={'/'}>Augustana Marketplace</a>
                 <a className="form-inline nav-link" href='/create/request'>Request</a>
                 <a className="form-inline nav-link" href='/create/sell'>Sell</a>
-                <a className="form-inline nav-link" href='/list/sell'>Sell Listings</a>
-                <a className="form-inline nav-link" href='/list/request'>Requested Listings</a>
-                <div className={"form-inline ml-5"}>
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </div>
-                <button className="" id="signout-btn" onClick={handleSignout}>Sign Out</button>
+                <a className="form-inline nav-link" href='/list/sell'>Listings</a>
+                <a className="form-inline nav-link" href='/list/request'>Requested Items</a>
+                <input className="form-control mr-sm-2 form-inline" type="search" placeholder="Search" aria-label="Search"/>
+                <button className="btn btn-outline-success form-inline my-2 my-sm-0" type="submit">Search</button>
+                <button className={""} id="signout-btn" onClick={handleSignout}>Sign Out</button>
             </form>
         </nav>
         </body>
