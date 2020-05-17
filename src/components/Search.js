163ad94e6fc0b4cd5056eed4365ff3d3a5725636
Lars Harvey <larsenharvey16@augustana.edu>
2020-05-17 16:34:26 -0500

    Added likes to item list and favorites pages

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,9 +15,47 @@ const searchClient = algoliasearch(
     );
 
 const HitComponent = ({hit}) => {
-    const requestOrSell = useSelector(state => state.categories.isSell)
+    const firebase = useFirebase();
+    const [liked, changeLikeStatus] = React.useState(hit.usersLike.includes(firebase.auth().currentUser.uid));
+
+    const addToFavorites = () => {
+        changeLikeStatus(true);
+        firebase.firestore().collection(requestOrSell ? 'sell' : 'request').doc(hit.objectID).get().then(async (doc) => {
+            const data = {...doc.data()};
+            data.usersLike.push(firebase.auth().currentUser.uid);
+            await firebase.firestore().collection(requestOrSell ? 'sell' : 'request').doc(hit.objectID).update(data)
+
+        })
+    };
+
+    const removeFromFavorites = () => {
+        changeLikeStatus(false);
+        firebase.firestore().collection(requestOrSell ? 'sell' : 'request').doc(hit.objectID).get().then(async (doc) => {
+            const data = {...doc.data()};
+            data.usersLike = data.usersLike.filter(user => user !== firebase.auth().currentUser.uid);
+            await firebase.firestore().collection(requestOrSell ? 'sell' : 'request').doc(hit.objectID).update(data)
+
+        })
+    };
+    const requestOrSell = useSelector(state => state.categories.isSell);
     return (
         <div className={"card card-rounded"}>
+            {hit.uid !== firebase.auth().currentUser.uid ?
+                <div className={"pt-2 pr-2 position-absolute right-0"}>
+                    <button onClick={liked ? removeFromFavorites : addToFavorites} className={"border rounded-circle outline-none border-grey like-btn"}>
+                        <div className={"px-1 py-1"}>
+                            {liked ? <h6 className={"m-0 pt-small"}>
+                                    <i className="fa fa-heart color-red"></i>
+                                </h6>
+                            : <h6 className={"m-0 pt-small"}>
+                                    <i className="fa fa-heart-o" aria-hidden="true"></i>
+                                </h6>
+                            }
+                        </div>
+                    </button>
+                </div>
+                : null}
+
             <div className={"container center-text card-height"}>
                 <img className="card-img-top w-50 pt-2 image-sizing" src={hit.imageUrl} />
             </div>
@@ -62,13 +100,13 @@ const Search = () => {
     <div>
         <NavBar/>
         <div className={"pt-4"}>
-            <h4 className={"d-inline-block mr-3 border-bottom border-primary"}>
+            <h4 className={"d-inline-block mr-4 border-bottom border-primary"}>
                 <Link className="text-decoration-none text-primary" to={`/search`}>All {isSell ? 'Listings' : 'Requests'}</Link>
             </h4>
-            <h4 className={"d-inline-block mr-3"}>
+            <h4 className={"d-inline-block mr-4"}>
                 <Link className="text-decoration-none text-muted" to={`/list/${isSell ? 'sell' : 'request'}/${firebase.auth().currentUser.uid}`}>My {isSell ? 'Listings' : 'Requests'}</Link>
             </h4>
-            <h4 className={"d-inline-block mr-3"}>
+            <h4 className={"d-inline-block mr-4"}>
                 <Link className={"text-decoration-none text-muted"} to={`/list/${isSell ? 'sell' : 'request'}/favorites`}>Favorite {isSell ? 'Listings' : 'Requests'}</Link>
             </h4>
         </div>
